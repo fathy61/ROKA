fa48cf8da3f9b406a3653e6a4c12cc203c08c496
Ahmed Fathy <ahmedfathy159930@gmail.com>
2026-01-01 04:54:15 +0200

    add whatsapp numbers

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const downloadCheckbox = document.getElementById('download');
 const downloadLink = document.getElementById('downloadLink');
 const wrapper = document.getElementById('page-wrapper');
+const whatsappBtn = document.querySelector('.whatsapp-btn');
+const whatsappModal = document.getElementById('whatsappModal');
+const closeModal = document.querySelector('.close-modal');
 
 downloadCheckbox.addEventListener('change', () => {
   if (downloadCheckbox.checked) {
@@ -16,7 +19,22 @@ window.addEventListener('load', () => {
     splash.classList.add('slide-up');
     setTimeout(() => {
       splash.style.display = 'none';
-      wrapper.classList.add('zoom-in'); // هنا يبدأ تأثير اللوجو ينزل
+      wrapper.classList.add('zoom-in');
     }, 1000); 
   }, 2000); 
+});
+
+whatsappBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  whatsappModal.classList.add('active');
+});
+
+closeModal.addEventListener('click', () => {
+  whatsappModal.classList.remove('active');
+});
+
+whatsappModal.addEventListener('click', (e) => {
+  if (e.target === whatsappModal) {
+    whatsappModal.classList.remove('active');
+  }
 });
